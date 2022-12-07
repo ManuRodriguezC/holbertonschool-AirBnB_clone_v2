47ade5c5039c5fcc4744a00800430a4d1c3dc43b
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-06 20:28:20 -0500

    Created and uses of the templates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -52,7 +52,7 @@ def number(n):
 @app.route('/number_template/<int:n>')
 def template_number(n):
     """Uses of the templates for pages"""
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
