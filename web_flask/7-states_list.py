fdb732d48ae00e4b00e1427a764dba247a4e82c1
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-09 11:46:24 -0500

    Task 7,8 all states in the db

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-"""This module give the dates of the list the states"""
+"""
+starts a Flask web app
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ This method return all states in the database"""
-    list_states = storage.all(State)
-    return render_template('7-states_list.html', list_states=list_states)
+    """display a HTML with the states of the db"""
+    states = storage.all(State)
+    return render_template(
+        '7-states_list.html',
+        states=sorted(
+            states.values(),
+            key=lambda x: x.name))
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Close current session"""
+def teardown_db(exception):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
