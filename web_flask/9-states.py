5e4d23bdd53d690e34e02f5bb6bf8ce7d7d62ffe
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-09 17:57:46 -0500

    Task 9, states with cities if exist

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
 """Conecting the flask with dbstorage, and return list states"""
-from flask import Flask, render_template
 from models import storage
 from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def states():
+@app.route("/states", strict_slashes=False)
+def state_list():
     """This method display list of the states"""
-    list_states = storage.all(State)
-    return render_template('9-states.html', list_states=list_states,\
+    list_state = storage.all(State)
+    return render_template("9-states.html", list_state=list_state,\
         mode="state")
 
 
-@app.route('states/<id>')
+@app.route("/states/<id>")
 def states_id(id):
     """This method display states for the id"""
-    list_states = storage.all(State)
-    for state in list_states.value():
+    for state in storage.all(State).values():
         if state.id == id:
-            return render_template('9-state.html', list_states,\
-                mode="state_id")
-    return render_template('9-states.html', list_states=list_states,\
-        mode="none")
+            return render_template("9-states.html", state=state,
+                                   mode="state_id")
+    return render_template("9-states.html", state=state, mode="none")
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(execption):
     """This method close the curren session"""
-    storage.close()
+    return storage.close()
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
