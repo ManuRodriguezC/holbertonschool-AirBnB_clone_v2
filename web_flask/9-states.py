3731b960465de98aa5971fa212e5d8524634a9b3
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-09 17:52:09 -0500

    Task 9, states with cities if exist

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
-"""This module give the dates of the list the states"""
+"""Conecting the flask with dbstorage, and return list states"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
 
 
-@app.route('states')
+@app.route('/states', strict_slashes=False)
 def states():
-    """"""
+    """This method display list of the states"""
     list_states = storage.all(State)
     return render_template('9-states.html', list_states=list_states,\
-        mode="list_states")
+        mode="state")
 
 
 @app.route('states/<id>')
 def states_id(id):
-    """This method return states and cities if exist"""
+    """This method display states for the id"""
     list_states = storage.all(State)
-    for state in list_states.values():
+    for state in list_states.value():
         if state.id == id:
-            return render_template('9-states.html', list_states=list_states,\
+            return render_template('9-state.html', list_states,\
                 mode="state_id")
     return render_template('9-states.html', list_states=list_states,\
         mode="none")
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close current session"""
+def teardown(exception):
+    """This method close the curren session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host="0.0.0.0")
