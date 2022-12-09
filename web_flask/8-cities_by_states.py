b4723f95f8962d06f4e6497951964f77ae2b5bd7
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-09 17:14:47 -0500

    Task 9, cities and states in db

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """
-This module give the dates of the list the states and cities
+This module give the dates of the list the states
 """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
 
-
 app = Flask(__name__)
 
 @app.route('cities_by_states')
 def cities_by_state():
     """This methos return od the cities and states in db"""
-    list_states = storage.all(State)
-    list_cities = storage.all(City)
-    return render_template('8-cities_by_states.html',\
-        list_cities=list_cities, list_states=list_states)
+    list_state = storage.all(State)
+    return render_template('8-cities_by_states.html', list_state=list_state)
 
 
 @app.teardown_appcontext
