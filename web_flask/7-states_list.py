9ef8ff9a307f4969c564b0406efcdb7aa7f7a781
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-09 11:34:59 -0500

    Task 7,8 all states in the db

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module give the dates of the list the states"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ This method return all states in the database"""
+    list_states = storage.all(State)
+    return render_template('7-states_list.html', list_states=list_states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Close current session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
