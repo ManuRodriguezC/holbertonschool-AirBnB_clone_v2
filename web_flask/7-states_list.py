f44cda52832fb21d1fb32123b7451ae73d93828b
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-09 17:21:32 -0500

    Task 9, cities and states in db

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This module give the dates of the list the states
-"""
+"""This module give the dates of the list the states"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,11 +7,11 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def display_cities_by_state():
     """ This method return all states in the database"""
-    list_states = storage.all(State)
-    return render_template('7-states_list.html', list_states=list_states)
+    list_state = storage.all(State)
+    return render_template('8-cities_by_states.html', list_state=list_state)
 
 
 @app.teardown_appcontext
@@ -23,4 +21,4 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
